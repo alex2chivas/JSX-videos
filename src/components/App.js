9041a0565a4a9d78aced2c5fc35265c2ccbeb4fd
Alexis Flores <alex2chivas@hotmail.com>
2020-03-15 10:21:35 -0700

    added classNames to call files & added functionality to SearchBar component & created a file to handle axios call, called youtube to fetch api

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import SearchBar from './SearchBar';
 export class App extends Component {
 	render() {
 		return (
-			<div>
+			<div className='ui container'>
 				<SearchBar />
 			</div>
 		);
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 
 export class SearchBar extends Component {
+	state = { term: '' };
+
+	onInputChange = (event) => {
+		this.setState({ term: event.target.value });
+	};
+
+	onFormSubmit = (event) => {
+		event.preventDefault();
+
+		// TODO:
+		// Callback from parent Component
+	};
+
 	render() {
-		return <div>Search Bar</div>;
+		return (
+			<div className='search-bar ui segment'>
+				<form className='ui form' onSubmit={this.onFormSubmit}>
+					<div className='field'>
+						<label htmlFor=''> Video Search</label>
+						<input
+							type='text'
+							placeholder='enter search'
+							value={this.state.term}
+							onChange={this.onInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+		);
 	}
 }
 
