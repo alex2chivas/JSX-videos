b212bfab964f7b49d93bea3e566e3c403a6ca618
Alexis Flores <alex2chivas@hotmail.com>
2020-03-16 18:37:14 -0700

    added scss files

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
+import '../src/components/main.scss';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 // NOTE
 //console.developer.google.com
