28444a5985ac0116970555e36f04b152185c37d4
Alexis Flores <alex2chivas@hotmail.com>
2020-03-16 19:30:36 -0700

    added VideoDetail component and added if statement to add a loading screen in way since the selectedVideo gets called before we ever select a video

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from './apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 export class App extends Component {
 	state = { videos: [], selectedVideo: null };
@@ -19,17 +20,21 @@ export class App extends Component {
 			this.setState({ videos: response.data.items });
 		} else {
 			console.log(' Error with the Api call for youtube videos ');
+			alert('The request for videos is currently not working, please try again later');
 		}
 	};
 
 	onVideoSelect = (video) => {
-		console.log('From the App!', video);
+		this.setState({
+			selectedVideo: video
+		});
 	};
 
 	render() {
 		return (
 			<div className='ui container'>
 				<SearchBar onTermSubmit={this.onTermSubmit} />
+				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
 			</div>
 		);
